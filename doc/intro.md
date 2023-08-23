6142f372f71140dba01999bbf18835022bdf2414
Kent Bull <kent@kentbull.com>
2023-08-22 21:49:11 -0600

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to keri-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kentbull/keri_clj_test.clj
@@ -0,0 +1,7 @@
+(ns kentbull.keri-clj-test
+  (:require [clojure.test :refer :all]
+            [kentbull.keri-clj :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
